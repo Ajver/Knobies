6abaecc02842c5ba63ac01b6bade4ceac4dea1b2
Ajver <zawlockidominik@gmail.com>
2019-10-21 13:41:31 +0200

    Disabling button + export

--- Game/UI/UI.gd
+++ Game/UI/UI.gd
@@ -1,12 +1,26 @@
 extends Node2D
 
+onready var buy_knob_btn = find_node("BuyKnobBtn")
 onready var score_label = find_node("Score")
 onready var knob_price_label = find_node("KnobPrice")
 onready var knobs_manager = get_node("/root/Game").find_node("KnobsManager")
 
+var buy_knob_btn_normal = preload("res://Game/UI/buy_knob_btn.png")
+var buy_knob_btn_pressed = preload("res://Game/UI/buy_knob_btn_pressed.png")
+var buy_knob_btn_disabled = preload("res://Game/UI/buy_knob_btn_disabled.png")
+
 func _ready():
 	GameData.connect("knob_price_changed", self, "update_knob_price_label")
+	GameData.connect("score_changed", self, "on_score_changed")
 	GameData.connect("score_changed", self, "update_score_label")
+
+func on_score_changed() -> void:
+	if GameData.can_buy_knob():
+		buy_knob_btn.normal = buy_knob_btn_normal
+		buy_knob_btn.pressed = buy_knob_btn_pressed
+	else:
+		buy_knob_btn.normal = buy_knob_btn_disabled
+		buy_knob_btn.pressed = null
 
 func update_score_label() -> void:
 	score_label.text = make_string(GameData.score)
@@ -22,7 +36,7 @@ func make_string(number:int) -> String:
 	else:
 		return str(floor(number/100000)/10.0, "m")
 
-func _on_BuyKnobBtn_pressed():
+func _on_BuyKnobBtn_released():
 	if GameData.can_buy_knob():
 		GameData.on_knob_buy()
 		knobs_manager.spawn_knob()
