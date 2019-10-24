2af023cf4c61b7cf86dedb3a1a151bc9a5e74df1
Ajver <zawlockidominik@gmail.com>
2019-10-24 19:50:08 +0200

    Change way of rounding numbers in UI.gd

--- Game/UI/UI.gd
+++ Game/UI/UI.gd
@@ -32,9 +32,9 @@ func make_string(number:int) -> String:
 	if number < 1000:
 		return str(number)
 	elif number < 1000000:
-		return str(floor(number/100)/10.0, "k")
+		return str(stepify(number/1000.0, 0.1), "k")
 	else:
-		return str(floor(number/100000)/10.0, "m")
+		return str(stepify(number/1000000.0, 0.1), "m")
 
 func _on_BuyKnobBtn_released():
 	if GameData.can_buy_knob():
